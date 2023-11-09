a99b5206108accc82d2fc69ffd2584c49b63df25
Cleyton Muto <cleytonmuto@gmail.com>
2023-11-09 15:38:47 -0300

    chore: change Link label

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ class App extends Component {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <Link to={"/tutorials"} className="navbar-brand">
-            Koiti
+            Vercel
           </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
